65d11ac115c26d9174ebb4398529dbc9476c9194
Erick_Dev <erickhenrique.rg@gmail.com>
2023-07-20 11:58:16 -0300

    App Page

--- src/app/PageSignupClient.jsx
+++ src/app/PageSignupClient.jsx
@@ -1,11 +1,22 @@
+import Button from '@/components/Fragment/Button'
 import React from 'react'
 
 const PageSignupClient = () => {
+    const newPrice = () => {}
     return (
         <div
             style={{ borderRadius: '80px' }}
             className="flex pt-20 justify-start  flex-col gap-[100px]"
         >
+            <img
+                src="./ImgApp/containerConfig.png"
+                className="absolute inset-0 top-1 first-letter left-6 pt-6 z-[-2]"
+            />
+            <img
+                src="./ImgApp/configImg.png"
+                className="absolute inset-0 first-letter left-10  top-3 pt-6 "
+            />
+
             <div className=" pt-20 ">
                 <img
                     src="./ImgApp/Hero.png"
@@ -35,7 +46,22 @@ const PageSignupClient = () => {
                     </h1>
                     <span class="text-center">
                         Use cupom para seu primeiro pedido:
-                        <strong className="tex-color"> MoToGo2023</strong>
+                        <strong className="text-orange uppercase">
+                            {' '}
+                            MoToGo2023
+                        </strong>
+                        <button className="m-auto p-5 block" onClick={newPrice}>
+                            <div className="relative">
+                                <img
+                                    src="./ImgApp/btnPedido.png"
+                                    className="m-auto"
+                                />
+                                <img
+                                    src="./ImgApp/open.png"
+                                    className="absolute inset-0 m-auto"
+                                />
+                            </div>
+                        </button>
                     </span>
                 </div>
             </div>
