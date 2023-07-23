8ddd95f56570c8ab170ec907c82636a62ac592d3
Erick_Dev <erickhenrique.rg@gmail.com>
2023-07-23 01:44:11 -0300

    PageSucess

--- src/app/Mochup.jsx
+++ src/app/Mochup.jsx
@@ -3,6 +3,7 @@ import Form from '../components/Forms/Form'
 import FormData from '../components/Forms/FormData'
 import PageLogin from './PageLogin'
 import PageSignupClient from './PageSignupClient'
+import PageSucess from './PageSucess'
 
 const Mochup = () => {
     const [type, setType] = useState('cadastro')
@@ -16,7 +17,7 @@ const Mochup = () => {
                 className="absolute -top-4  h-[782px] w-[480px] z-[-1] scale-{120} "
             ></img>
 
-            {type == 'cadastrso' ? <PageLogin /> : <PageSignupClient />}
+            {type == 'cadastrso' ? <PageLogin /> : <PageSucess />}
         </div>
     )
 }
---
+++ src/app/PageSucess.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PageSucess = () => {
+    return (
+        <div>
+            <img
+                src="./ImgApp/Union.png"
+                className="absolute z-[-2] top-0 left-0"
+            ></img>
+            <img src="./ImgApp/entregador.png" className="z-[-2]" />
+            <h1 className="font-bold text-center  text-4xl">
+                Cadastro
+                <br />
+                Concluido
+            </h1>
+            <span className=" mb-5 block text-center"></span>
+        </div>
+    )
+}
+
+export default PageSucess
