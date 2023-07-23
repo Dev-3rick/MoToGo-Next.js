2134952d3c04733d9ed68fddf1055ad5c1d8411d
Erick_Dev <erickhenrique.rg@gmail.com>
2023-07-23 15:11:47 -0300

    PagApp

--- src/app/Mochup.jsx
+++ src/app/Mochup.jsx
@@ -4,7 +4,7 @@ import FormData from '../components/Forms/FormData'
 import PageLogin from './PageLogin'
 import PageSignupClient from './PageSignupClient'
 import PageSucess from './PageSucess'
-
+import PagApp from './PagApp'
 const Mochup = () => {
     const [type, setType] = useState('cadastro')
     return (
@@ -17,7 +17,7 @@ const Mochup = () => {
                 className="absolute -top-4  h-[782px] w-[480px] z-[-1] scale-{120} "
             ></img>
 
-            {type == 'cadastrso' ? <PageLogin /> : <PageSucess />}
+            {type == 'cadastrso' ? <PageLogin /> : <PagApp />}
         </div>
     )
 }
