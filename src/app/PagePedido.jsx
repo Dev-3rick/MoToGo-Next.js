b70ac310fb5707cb18902f6c9280215a3f7715b8
Erick_Dev <erickhenrique.rg@gmail.com>
2023-07-25 20:01:07 -0300

    PagePedido

--- src/app/PagePedido.jsx
+++ src/app/PagePedido.jsx
@@ -1,7 +1,13 @@
+import FormPedidos from '@/components/Forms/FormPedidos'
 import React from 'react'
 
 const PagePedido = () => {
-    return <div></div>
+    return (
+        <div>
+            <h1 className="font-bold text-center pb-10  text-4xl">Coleta</h1>
+            <FormPedidos />
+        </div>
+    )
 }
 
 export default PagePedido
--- src/components/Forms/FormPedidos.jsx
+++ src/components/Forms/FormPedidos.jsx
@@ -1,16 +1,108 @@
 import React from 'react'
 import Input from '../Fragment/Input'
+import Button from '../Fragment/Button'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 const FormPedidos = () => {
+    const [logradouroColeta, setLogradouroColeta] = React.useState('')
+    const [numero, setNumero] = React.useState('')
+    const [obs, setObs] = React.useState('')
+
+    const [logradouroEntrega, setLogradouroEntrega] = React.useState('')
+    const [numeroEntrega, setNumeroEntrega] = React.useState('')
+    const [referenciaEntrega, setReferenciaEntrega] = React.useState('')
+
+    const [mostrarEntrega, setMostrarEntrega] = React.useState(true)
+
+    const FormPedidoSubmit = (event) => {
+        event.preventDefault()
+    }
+
     return (
-        <form>
-            <Input
-                id=""
-                placeholder=""
-                setValue=""
-                type=""
-                key=""
-                value=""
-            ></Input>
+        <form
+            className="flex flex-col items-center pb-10 gap-2"
+            onSubmit={FormPedidoSubmit}
+        >
+            <GooglePlacesAutocomplete
+                apiKey="AIzaSyDVJoBo28gnJ4CdYo1m5cdJZdR6_uBojOM"
+                selectProps={{
+                    logradouroColeta,
+                    onChange: setLogradouroColeta,
+                }}
+            />
+            {mostrarEntrega ? (
+                <>
+                    <Input
+                        id="LogadouroColeta"
+                        label="Logadouro de Coleta"
+                        type="text"
+                        value={logradouroColeta}
+                        setValue={setLogradouroColeta}
+                    />
+                    <Input
+                        id="NumColeta"
+                        label="Numero"
+                        type="number"
+                        value={numero}
+                        setValue={setNumero}
+                    />
+                    <Input
+                        id="EndEntrega"
+                        label="Referencia"
+                        type="text"
+                        value={obs}
+                        setValue={setObs}
+                    />{' '}
+                    <label htmlFor="obs" className="mr-auto">
+                        Obs:
+                    </label>
+                    <textarea
+                        name="Obs"
+                        id="obs"
+                        cols="24"
+                        rows="2"
+                        className={`shadow- p-3 rounded-md border border-zinc-400 bg-zinc-100 outline-none`}
+                    ></textarea>{' '}
+                    <Button
+                        color="bg-[#008AFF]/60 "
+                        text="Continuar"
+                        type="button"
+                        setClick={() => {
+                            setMostrarEntrega(
+                                (mostrarEntrega) => !mostrarEntrega
+                            )
+                        }}
+                    />
+                </>
+            ) : (
+                <>
+                    <Input
+                        id="LogadouroEntrega"
+                        label="Logadouro de Entrega"
+                        type="text"
+                        value={logradouroEntrega}
+                        setValue={setLogradouroEntrega}
+                    />
+                    <Input
+                        id="NumEntrega"
+                        label="Numero"
+                        type="number"
+                        value={numeroEntrega}
+                        setValue={setNumeroEntrega}
+                    />
+                    <Input
+                        id="RefEntrega"
+                        label="Referencia"
+                        type="text"
+                        value={referenciaEntrega}
+                        setValue={setReferenciaEntrega}
+                    />{' '}
+                    <Button
+                        color="bg-[#008AFF]/60 "
+                        text="Continuar"
+                        type="submit"
+                    />
+                </>
+            )}
         </form>
     )
 }
